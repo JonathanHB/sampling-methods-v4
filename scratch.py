b0a81ed3762107373a90e9102fb40fe07d7dfb08
Jonathan Borowsky <jonathan3141@gmail.com>
2026-08-03 14:19:30 -0700

    cleaned code, wrote tests, at step 3 of scratch.py

--- scratch.py
+++ scratch.py
@@ -10,7 +10,7 @@ To do list
     manually clean and review current code
     feed cleaned code to claude and ask it to check it; also upload huber and kim 1996 
 4. make a metadynamics wrapper function matching the WE propagator requirements
-5. make simulation system object (and decide if this actually makes sense)
+5. make simulation system object (and decide if this actually makes sense) w/ macrostate classifier (or make the classifier its own thing with the CV?)
 6. write function to set MTD parameters based on the existing one in msm_toy_systems/
     implement binless diffusion coefficient calculation and see if it matches MSM
 7. assemble all of this in main and test it
